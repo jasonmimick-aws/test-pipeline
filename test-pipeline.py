6b4841a96bddcc7842140d21231e8b1b0a8be362
Jason Mimick <mimjasov@amazon.com>
2023-04-29 09:11:09 -0400

    fix app

--- test-pipeline.py
+++ test-pipeline.py
@@ -4,5 +4,6 @@ print("rocking new feature")
 print("rocking new feature 2")
 print("rocking new feature 3")
 print("rocking new feature 4")
+print("rocking new feature 5")
 
 print(f"Hello test-pipeline {time.time()}")
